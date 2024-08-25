b35fd6284ce7d3c7818135df04ddda6cf6305e86
dmitriyimamov@gmail.com <dmitriyimamov@gmai.com>
2024-08-25 14:39:29 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    return Number(String(n).slice(1).split('').reverse().join(''));
+  } else {
+    return Number(String(n).split('').reverse().join(''));
+  }
 }
